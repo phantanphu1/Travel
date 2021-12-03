c0f8566cbcb0255d15276ade34afbba24b1c08c5
phanhungphong <phanhungphong.qn@gmail.com>
2021-12-03 23:01:25 +0700

    update file and design card-product ,login

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,13 @@
+import "./login.scss";
+// import {useHistory} from 'react-router-dom';
+
+function Login(){
+    // const history=useHistory();
+
+    return(
+<div className="login-container">
+    <h1>kk</h1>
+</div>
+    )
+}
+export default Login;
